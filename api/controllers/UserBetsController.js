d96b705d86472c2b23cffda0876b574f9afa62b7
prajaktakamble <prajakta.kamble@wohlig.com>
2018-03-28 19:05:08 +0530

    while saving game result uspdte winning anount in win users functionality done

--- api/controllers/UserBetsController.js
+++ api/controllers/UserBetsController.js
@@ -12,5 +12,17 @@ var controller = {
             })
         }
     },
+    saveGameResults: function (req, res) {
+        if (req.body) {
+            UserBets.saveGameResults(req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: {
+                    message: "Invalid Request"
+                }
+            })
+        }
+    },
 };
 module.exports = _.assign(module.exports, controller);
--- api/services/UserBets.js
+++ api/services/UserBets.js
@@ -12,6 +12,11 @@ var schema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Bets"
     },
+    winningAmount: {
+        type: Number,
+        default: 0
+    }
+
 });
 
 schema.plugin(deepPopulate, {
@@ -40,8 +45,8 @@ var model = {
      * @param {model} input modelname/serviceName
      * @param {callback} callback function with err and response
      */
-    callApiToSave: function (data, model, callback) {
-        console.log("data requestSend", data)
+    callApiToSave: function (userBets, model, callback) {
+        console.log("data requestSend", userBets)
         var url = Config.backendUrl;
 
         var options = {
@@ -57,7 +62,6 @@ var model = {
             function (err, httpResponse, body) {
 
                 if (body) {
-
                     callback(err, body.data);
                 } else {
                     callback("Some Error");
@@ -74,26 +78,121 @@ var model = {
      */
     saveUserBets: function (userBets, callback) {
         console.log("in saveUserBets", userBets);
-        UserBets.saveData(userBets, function (err, savedData) {
-            if (err) {
-                console.log("err in saving userbets", err);
-                callback(err, null);
-            } else if (_.isEmpty(savedData)) {
-                callback(null, false);
-            } else {
-                model = "Member/deductMoney";
-                console.log("$$$$$$$$$$$$$$$$$$", savedData);
-                var userBets = {
-                    user: savedData.user,
-                    amountplaces: savedData.amountplaces
-                }
-                UserBets.callApiToSave(userBets, model, function (data) {
-                    console.log("@@@", data);
-                    callback(null, data);
-                });
+        Game.findOne({
+            "results": {
+                $exists: false
             }
+        }).lean().exec(function (err, game) {
+            userBets.game = game._id;
+            UserBets.saveData(userBets, function (err, savedData) {
+                if (err) {
+                    console.log("err in saving userbets", err);
+                    callback(err, null);
+                } else if (_.isEmpty(savedData)) {
+                    callback(null, false);
+                } else {
+                    var model = "Member/deductMoney";
+                    var userBets = {
+                        user: savedData.user,
+                        amountplaces: savedData.amountplaces
+                    }
+                    UserBets.callApiToSave(userBets, model, function (data) {
+                        console.log("@@@", data);
+                        callback(null, data);
+                    });
+                }
+            });
         });
     },
 
+    /**
+     * this function to save game result
+     * @param {gameResult.result} input results
+     * @param {callback} callback function with err and response
+     */
+    saveGameResults: function (gameResult, callback) {
+        Game.findOneAndUpdate({
+            "results": {
+                $exists: false
+            }
+        }, {
+            $set: {
+                results: gameResult.results
+            }
+        }, {
+            new: true
+        }).lean().exec(function (err, game) {
+            if (err) {
+                console.log("err in save game results", err);
+            } else if (!_.isEmpty(game)) {
+                UserBets.find({
+                    "game": game._id
+                }).lean().deepPopulate("bet").exec(function (err, usersbets) {
+                    if (err) {
+                        console.log("error in finding userBets", usersbets);
+                    } else if (!_.isEmpty(usersbets)) {
+                        var betUsers = {};
+                        var winUser = [];
+                        var loseUser = [];
+                        async.each(usersbets, function (user, cb1) {
+                            var number = user.bet.numbers;
+                            if (number.indexOf(game.results) != -1) {
+                                console.log("in if", user.bet);
+                                var user1 = {
+                                    user: user.user,
+                                    amountplaces: user.amountplaces,
+                                    multiplier: user.bet.multiplier,
+                                    winningAmount: user.amountplaces * user.bet.multiplier
+                                }
+                                winUser.push(user1);
+                                UserBets.findOneAndUpdate({
+                                    user: user.user,
+                                    bet: user.bet._id,
+                                    game: game._id
+                                }, {
+                                    $set: {
+                                        winningAmount: user.amountplaces * user.bet.multiplier
+                                    }
+                                }, {
+                                    new: true
+                                }).lean().exec(function (err, updatedUserBets) {
+                                    if (err) {
+                                        callback(err, null);
+                                    } else if (!_.isEmpty(updatedUserBets)) {
+                                        cb1();
+                                    }
+                                });
+                            } else {
+                                console.log("in else");
+                                var user2 = {
+                                    user: user.user,
+                                    amountplaces: user.amountplaces,
+                                    multiplier: user.bet.multiplier,
+                                    winningAmount: 0
+                                }
+                                loseUser.push(user2);
+                                cb1();
+                            }
+                        }, function (err) {
+                            if (err) {
+                                callback(err, null);
+                            } else {
+                                // UserBets.callApiToSave(winUser, "Member/deductMoney", function (data) {
+                                //     console.log("@@@", data);
+                                //     callback(null, data);
+                                // });
+
+                                betUsers.loseUser = loseUser;
+                                betUsers.winUser = winUser;
+                                callback(null, betUsers);
+                            }
+                        });
+                    } else {
+                        callback(null, false);
+                    }
+                });
+            }
+        })
+    }
 };
 module.exports = _.assign(module.exports, exports, model);
